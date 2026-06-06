f9cf26b6209be921a49f66f085a92c68946662f0
Harshvardhan <harshvardhanpipariya65@example.com>
2026-06-07 01:54:39 +0530

    change in frontend url cors

--- src/app.js
+++ src/app.js
@@ -14,14 +14,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://whats-app-frontend-4f8g38qc0-harshvardhan-pipariyas-projects.vercel.app",
+    origin: "https://whats-app-frontend-git-main-harshvardhan-pipariyas-projects.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 app.use(cors({
-  origin: "https://whats-app-frontend-4f8g38qc0-harshvardhan-pipariyas-projects.vercel.app",
+  origin: "https://whats-app-frontend-git-main-harshvardhan-pipariyas-projects.vercel.app",
   credentials: true,
 }));
 
