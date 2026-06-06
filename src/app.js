1ea8ee989757fde9e499cb9ef0873ff1c1cf406f
Harshvardhan <harshvardhanpipariya65@example.com>
2026-06-07 01:47:47 +0530

    correction cors

--- src/app.js
+++ src/app.js
@@ -14,14 +14,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://whats-app-frontend-4f8g38qc0-harshvardhan-pipariyas-projects.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://whats-app-frontend-4f8g38qc0-harshvardhan-pipariyas-projects.vercel.app",
   credentials: true,
 }));
 
